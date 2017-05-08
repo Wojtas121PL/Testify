e888c635b49b17769fa5721c483d1e419c165de8
krecix1213 <x8cegla@gmail.com>
2017-05-08 16:02:12 +0200

    Fish was eat for dinner :/ Seriously New Table Seeder For Checked Answers

--- Testify/database/seeds/testsTableSeeder.php
+++ Testify/database/seeds/testsTableSeeder.php
@@ -20,10 +20,10 @@ class testsTableSeeder extends Seeder
             'QuestionType' => 0,
             'Question' => 'Why does a chicken cross a road?',
             'Answers' => json_encode([
-                "A" => "asdas",
-                "B" => "sasgoiaj",
-                "C" => "sasgoiaj",
-                "D" => "sasgoiaj",
+                "A" => "Yes",
+                "B" => "No",
+                "C" => "Chicken is stupid",
+                "D" => "I eat its for dinner",
             ]),
             'AnswerKey' => 4
         ]);
@@ -31,12 +31,12 @@ class testsTableSeeder extends Seeder
             'TestId' => 1,
             'QuestionId' => 2,
             'QuestionType' => 0,
-            'Question' => 'Why does a chicken cross a road?',
+            'Question' => 'Why does a dog cross a road?',
             'Answers' => json_encode([
-                "A" => "asdas",
-                "B" => "sasgoiaj",
-                "C" => "sasgoiaj",
-                "D" => "sasgoiaj",
+                "A" => "Yes",
+                "B" => "No",
+                "C" => "Dog is stupid",
+                "D" => "I eat its for dinner",
             ]),
             'AnswerKey' => 4
         ]);
@@ -44,12 +44,12 @@ class testsTableSeeder extends Seeder
             'TestId' => 2,
             'QuestionId' => 1,
             'QuestionType' => 0,
-            'Question' => 'Why does a chicken cross a road?',
+            'Question' => 'Why does a fish cross a road?',
             'Answers' => json_encode([
-                "A" => "asdas",
-                "B" => "sasgoiaj",
-                "C" => "sasgoiaj",
-                "D" => "sasgoiaj",
+                "A" => "Yes",
+                "B" => "No",
+                "C" => "Fish is stupid",
+                "D" => "I eat its for dinner",
             ]),
             'AnswerKey' => 4
         ]);
@@ -57,12 +57,12 @@ class testsTableSeeder extends Seeder
             'TestId' => 2,
             'QuestionId' => 2,
             'QuestionType' => 0,
-            'Question' => 'Why does a chicken cross a road?',
+            'Question' => 'Why does a cat cross a road?',
             'Answers' => json_encode([
-                "A" => "asdas",
-                "B" => "sasgoiaj",
-                "C" => "sasgoiaj",
-                "D" => "sasgoiaj",
+                "A" => "Yes",
+                "B" => "No",
+                "C" => "Cat is stupid",
+                "D" => "I eat its for dinner",
             ]),
             'AnswerKey' => 4
         ]);
