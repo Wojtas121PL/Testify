cd8779fba72620646f716c4452072fe3d0ee1d7d
krecix1213 <x8cegla@gmail.com>
2017-08-17 16:03:27 +0200

    Zastępowanie starych bugów, nowymi.
    Dzień w którym laravel zniszczył mi życie :D
    
    -Nowa migracja
    -Wstępniak do wyświetlania rezultatów dla pytań wielkokrotnego wyboru

---
+++ Modules/Exam/Database/Migrations/2017_08_17_140238_create_multi_answer_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMultiAnswerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('multi_answer', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('id_answer_group');
+            $table->unsignedInteger('answer');
+            $table->foreign('answer')->references('id')->on('answers')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('multi_answer');
+    }
+}
